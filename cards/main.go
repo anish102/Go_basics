a3214d98520142214e16752d4c6677b14e309e85
Anish Karki <anishkarki695@gmail.com>
2023-03-30 21:01:43 +0545

    Arrays and For loops

--- cards/main.go
+++ cards/main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	//var card string = "Ace of Spades"
-	card := "Ace of spades"
-	card = "Five of diamonds"
+	cards := []string{"Ace of diamonds", newCard()}
+	cards = append(cards, "Six of spades")
 
-	fmt.Println(card)
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Five of diamonds"
 }
