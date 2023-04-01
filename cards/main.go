e12f626bd7d3b82b01062fd80a05b04751e07f52
Anish Karki <anishkarki695@gmail.com>
2023-04-01 10:42:08 +0545

    Added Shuffle function

--- cards/main.go
+++ cards/main.go
@@ -1,7 +1,10 @@
 package main
 
 func main() {
-	cards := newDeckFromFile("mycards")
-	cards.print()
+	//cards := newDeckFromFile("mycards")
+	//cards.print()
 	//cards.saveToFile("my_cards")
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
 }
