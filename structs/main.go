ff0f06b9899c01d1c88a5c396cbfa1ef861ab810
Anish Karki <anishkarki695@gmail.com>
2023-04-02 20:36:03 +0545

    Nested structs

--- structs/main.go
+++ structs/main.go
@@ -1,13 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	name := person{firstName: "Anish", lastName: "Karki"}
-	fmt.Println(name)
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 940121,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 }
